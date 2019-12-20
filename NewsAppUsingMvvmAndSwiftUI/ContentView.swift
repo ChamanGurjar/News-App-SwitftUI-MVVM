a58dc270956ce863d12043920433952776765df2
ChamanGurjar <chamangujjar162@gmail.com>
2019-12-20 08:47:56 +0530

    Initial Commit

---
+++ NewsAppUsingMvvmAndSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsAppUsingMvvmAndSwiftUI
+//
+//  Created by ChamanGurjar on 20/12/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

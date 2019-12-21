7b8f086d039b20a076c21212ecbde60d87ea0ee9
ChamanGurjar <chamangujjar162@gmail.com>
2019-12-21 18:58:34 +0530

    Models Added.

---
+++ NewsAppUsingMvvmAndSwiftUI/Model/Article.swift
@@ -0,0 +1,14 @@
+//
+//  Article.swift
+//  NewsAppUsingMvvmAndSwiftUI
+//
+//  Created by ChamanGurjar on 21/12/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import Foundation
+
+struct Article: Codable {
+    let title: String
+    let description: String?
+}
---
+++ NewsAppUsingMvvmAndSwiftUI/Model/NewsResponse.swift
@@ -0,0 +1,15 @@
+//
+//  NewsResponse.swift
+//  NewsAppUsingMvvmAndSwiftUI
+//
+//  Created by ChamanGurjar on 21/12/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import Foundation
+
+
+struct NewsResponse: Codable {
+    let articles: [Article]
+    
+}

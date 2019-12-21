6d526301d33f8f25721a0f5214c6ca178bbe5e24
ChamanGurjar <chamangujjar162@gmail.com>
2019-12-21 19:01:21 +0530

    Webservice Implemented

---
+++ NewsAppUsingMvvmAndSwiftUI/WebService.swift
@@ -0,0 +1,31 @@
+//
+//  WebService.swift
+//  NewsAppUsingMvvmAndSwiftUI
+//
+//  Created by ChamanGurjar on 20/12/19.
+//  Copyright © 2019 ChamanGurjar. All rights reserved.
+//
+
+import Foundation
+
+class WebService {
+    
+    func loadTopHeadlines(url: URL, completion: @escaping ([Article]?) -> () ) {
+        
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            
+            guard let data = data, error == nil else {
+                completion(nil)
+                return
+            }
+            
+            let response = try? JSONDecoder().decode(NewsResponse.self, from: data)
+            if let response = response {
+                DispatchQueue.main.async {
+                    completion(response.articles)
+                }
+            }
+        }
+    }
+    
+}
